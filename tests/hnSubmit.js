177081f8712afbfd1f4c228e30b033a3e21b3cef
camziny <cameronziny@gmail.com>
2024-05-28 11:17:31 -0400

    - hacker news logged in to submit

---
+++ tests/hnSubmit.js
@@ -0,0 +1,12 @@
+// launch browser and go to Hacker News
+const { context } = await launch();
+const page = await context.newPage();
+await page.goto("https://news.ycombinator.com/");
+
+// click on the submit button
+await page.locator('[href="submit"]').click();
+
+// assert that the user must be logged in to submit
+await expect(page.locator(`body`)).toContainText(
+  "You have to be logged in to submit"
+);
