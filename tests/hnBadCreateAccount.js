90f2ec9f4ffaef1130a672824191388d537bf84e
camziny <cameronziny@gmail.com>
2024-05-29 11:43:51 -0400

    - update hacker news create account

--- tests/hnBadCreateAccount.js
+++ tests/hnBadCreateAccount.js
@@ -4,20 +4,23 @@ const page = await context.newPage();
 await page.goto("https://news.ycombinator.com/");
 
 // navigate to log in / create account page
-await page.locator(`[href="login?goto=news"]`).click();
+await page.getByRole("link", { name: "login" }).click();
 
 // fill in invalid username and password and click create account
 await page
-  .locator(`form:nth-of-type(2) [type="text"]`)
+  .locator(`form:has-text('create account') [type="text"]`)
   .fill(`cameronziny@gmail.co`);
 await page
-  .locator(`form:nth-of-type(2) [type="password"]`)
+  .locator(`form:has-text('create account') [type="password"]`)
   .fill(`testpassword`);
-await page.locator(`[value="create account"]`).click();
+// await page.locator(`[value="create account"]`).click();
+await page.getByRole("button", { name: "create account" }).click();
 
 // click reload button on error page
 await page.waitForTimeout(1000);
-await page.locator(`:text("reload")`).click();
+if (await page.locator(`:text("reload")`).isVisible()) {
+  await page.locator(`:text("reload")`).click();
+}
 
 // assert that the account cannot be created with an invalid username
 await expect(page.locator(`body`)).toContainText(
