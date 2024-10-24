3ec3f5e6e9a5d996ffb5c64baf459e1867727ef0
camziny <cameronziny@gmail.com>
2024-10-24 18:13:21 -0400

    Remove element
    - the-internet add and remove element

--- tests/tiAddRemoveElement.js
+++ tests/tiAddRemoveElement.js
@@ -1,17 +1,23 @@
-import { test, expect } from "@playwright/test";
+const { test, expect } = require("@playwright/test");
 
 test("Add/Remove Element Test", async ({ page }) => {
+  // Go to the-internet page
   await page.goto("https://the-internet.herokuapp.com/");
+
+  // Navigate to Checkboxes
   await page.getByRole("link", { name: "Checkboxes" }).click();
 
+  // Verify that the second checkbox is checked and the first one is not
   const checkboxOne = page.locator(`input[type="checkbox"]`).nth(0);
   const checkboxTwo = page.locator(`input[type="checkbox"]`).nth(1);
   await expect(checkboxTwo).toBeChecked();
   await expect(checkboxOne).not.toBeChecked();
 
+  // Click checkbox 1 and checkbox 2
   await checkboxOne.click();
   await checkboxTwo.click();
 
+  // Assert that checkbox 1 is checked and checkbox 2 is not
   await expect(checkboxOne).toBeChecked();
   await expect(checkboxTwo).not.toBeChecked();
 });
